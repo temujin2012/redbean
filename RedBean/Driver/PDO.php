d5dab3f41eaf75e44f67315614ffad65da5590da
Gabor de Mooij <gabordemooij@gmail.com>
2014-07-27 09:42:47 +0200

    Cherry-picked workaround 5.3 SQLite issue from master.

--- RedBean/Driver/PDO.php
+++ RedBean/Driver/PDO.php
@@ -15,6 +15,11 @@
 class RedBean_Driver_PDO implements RedBean_Driver
 {
 	/**
+	 * @var integer
+	 */
+	protected $max;
+
+	/**
 	 * @var string
 	 */
 	protected $dsn;
@@ -85,7 +90,7 @@ class RedBean_Driver_PDO implements RedBean_Driver
 			if ( is_integer( $key ) ) {
 				if ( is_null( $value ) ) {
 					$statement->bindValue( $key + 1, NULL, PDO::PARAM_NULL );
-				} elseif ( !$this->flagUseStringOnlyBinding && RedBean_QueryWriter_AQueryWriter::canBeTreatedAsInt( $value ) && $value <= PHP_INT_MAX ) {
+				} elseif ( !$this->flagUseStringOnlyBinding && RedBean_QueryWriter_AQueryWriter::canBeTreatedAsInt( $value ) && $value <= $this->max ) {
 					$statement->bindParam( $key + 1, $value, PDO::PARAM_INT );
 				} else {
 					$statement->bindParam( $key + 1, $value, PDO::PARAM_STR );
@@ -93,7 +98,7 @@ class RedBean_Driver_PDO implements RedBean_Driver
 			} else {
 				if ( is_null( $value ) ) {
 					$statement->bindValue( $key, NULL, PDO::PARAM_NULL );
-				} elseif ( !$this->flagUseStringOnlyBinding && RedBean_QueryWriter_AQueryWriter::canBeTreatedAsInt( $value ) && $value <= PHP_INT_MAX ) {
+				} elseif ( !$this->flagUseStringOnlyBinding && RedBean_QueryWriter_AQueryWriter::canBeTreatedAsInt( $value ) && $value <= $this->max ) {
 					$statement->bindParam( $key, $value, PDO::PARAM_INT );
 				} else {
 					$statement->bindParam( $key, $value, PDO::PARAM_STR );
@@ -226,6 +231,13 @@ class RedBean_Driver_PDO implements RedBean_Driver
 			$this->dsn = $dsn;
 
 			$this->connectInfo = array( 'pass' => $pass, 'user' => $user );
+		}
+		
+		//PHP 5.3 PDO SQLite has a bug with large numbers:
+		if ( strpos( $dsn, 'sqlite' ) === 0 && PHP_MAJOR_VERSION === 5 && PHP_MINOR_VERSION === 3) {
+			$this->max = 2147483647; //otherwise you get -2147483648 ?! demonstrated in build #603 on Travis.
+		} else {
+			$this->max = PHP_INT_MAX; //the normal value of course (makes it possible to use large numbers in LIMIT clause)
 		}
 	}
 
